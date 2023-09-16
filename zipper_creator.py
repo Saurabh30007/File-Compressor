9f50ce3906a0adba5a2f01497903cd438e337529
SAM <Sau3007@outlook.com>
2023-09-16 15:03:15 +0530

    inserted doc string for make_archive function

--- zipper_creator.py
+++ zipper_creator.py
@@ -3,6 +3,14 @@ import pathlib
 
 
 def make_archive(filepaths, dest_dir):
+
+    """
+    This fucnction convert file(s) to zip format
+    :param filepaths: directory for the origin file(s)
+    :param dest_dir: destination folder
+    :return:
+    """
+
     dest_path = pathlib.Path(dest_dir, 'compressed.zip')
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
